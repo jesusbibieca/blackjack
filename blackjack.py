67731b4cfdf80c83c2f17e35a40f5488ed9aa1aa
Jesus Bibieca <jesusbibieca52@gmail.com>
2018-01-14 19:41:05 -0400

    add test cases

--- blackjack.py
+++ blackjack.py
@@ -6,20 +6,22 @@
 import random as R
 
 class Card(object):
-	def __init__(self):
-		self.color = None
+	def __init__(self, color):
+		self.color = color
 		self.number =  R.choice(['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'])
+		
+		def set_value(number):
+			val = {'A': 11,
+				   'J': 10,
+				   'Q': 10,
+				   'K': 10}
+			if number in val:
+				return val[number]
+			else:
+				return number
+
 		self.value = set_value(self.number)
 
-	def set_value(self, number):
-		val = {'A': 11,
-			   'J': 10,
-			   'Q': 10,
-			   'K': 10}
-		if number in val:
-			return val[number]
-		else:
-			return number
 
 	def get_color(self):
 		return self.color
@@ -35,3 +37,8 @@ class Card(object):
 
 	def __str__(self):
 		return get_card(self)
+
+
+#Testing Card 
+cardA = Card('black')
+print "The card is a {} {} and it's value is {}".format(cardA.get_color(), cardA.get_number(), cardA.get_value())
